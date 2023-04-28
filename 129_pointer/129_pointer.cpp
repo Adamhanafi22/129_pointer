464d60b92aa6d5590b02ae30acdb84518344dc37
Adamhanafi22 <adamhanafi1922@gmail.com>
2023-04-28 16:35:08 +0700

    membuat in main memeber operator dan pointer

--- 129_pointer/129_pointer.cpp
+++ 129_pointer/129_pointer.cpp
@@ -10,3 +10,21 @@ public:
 void mahasiswa::showNim() {
 	cout << "No induk = " << nim << endl; //implementasi methoud diluar class
 }
+
+int main()
+{
+	mahasiswa mhs{ 1 }; //object mhs
+	mhs.showNim();      // member Acces operation
+
+	mahasiswa& ref = mhs; //pointer reference refmhs
+	ref.nim = 2;          //Member Access operation
+	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs; //pointer Dereferense pMhs
+	pMhs->nim = 3;          //Arrow operation
+	mhs.showNim();
+	system("pause");
+	return 0;
+
+
+}
