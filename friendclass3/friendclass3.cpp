a3010aa5e3ca1386ed43aaf308b55d5e41f269cd
Adamhanafi22 <adamhanafi1922@gmail.com>
2023-04-28 17:36:58 +0700

    membuat class pelajar,membuat metoud di luar class membuat int main

---
+++ friendclass3/friendclass3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class pelajar;
+class manusia {
+public:
+	void showNilaipelajar(pelajar&);
+};
+class pelajar {
+private:
+	int nilai;
+public:
+	pelajar() {
+		nilai = 100;
+
+	}
+	friend void manusia::showNilaipelajar(pelajar& x);
+};
+
+void manusia::showNilaipelajar(pelajar& x) {
+	cout << "Nilai pelajar: " << x.nilai;
+}
